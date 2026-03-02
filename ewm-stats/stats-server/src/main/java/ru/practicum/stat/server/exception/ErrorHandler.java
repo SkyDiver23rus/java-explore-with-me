14ec714aecb8c3fe2ae03380b6b21b3d7fa12734
SkyDiver <lvv10@yandex.ru>
2026-03-02 20:59:59 +0300

    fix postman 18

--- ewm-stats/stats-server/src/main/java/ru/practicum/stat/server/exception/ErrorHandler.java
+++ ewm-stats/stats-server/src/main/java/ru/practicum/stat/server/exception/ErrorHandler.java
@@ -2,23 +2,32 @@ package ru.practicum.stat.server.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler({
+            IllegalArgumentException.class,
+            MissingServletRequestParameterException.class,
+            MethodArgumentTypeMismatchException.class,
+            MethodArgumentNotValidException.class
+    })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleBadRequest(final IllegalArgumentException e) {
+    public ApiError handleBadRequest(final Exception e) {
         log.error("Bad request: {}", e.getMessage());
         return ApiError.builder()
-                .errors(Collections.singletonList(e.getStackTrace()[0].toString()))
+                .errors(Collections.singletonList(e.getClass().getSimpleName()))
                 .message(e.getMessage())
                 .reason("Incorrectly made request.")
                 .status(HttpStatus.BAD_REQUEST.name())
@@ -26,12 +35,12 @@ public class ErrorHandler {
                 .build();
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleNotFound(final RuntimeException e) {
         log.error("Not found: {}", e.getMessage());
         return ApiError.builder()
-                .errors(Collections.singletonList(e.getStackTrace()[0].toString()))
+                .errors(Collections.singletonList(e.getClass().getSimpleName()))
                 .message(e.getMessage())
                 .reason("The required object was not found.")
                 .status(HttpStatus.NOT_FOUND.name())
@@ -39,12 +48,12 @@ public class ErrorHandler {
                 .build();
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ApiError handleConflict(final Exception e) {
         log.error("Conflict: {}", e.getMessage());
         return ApiError.builder()
-                .errors(Collections.singletonList(e.getStackTrace()[0].toString()))
+                .errors(Collections.singletonList(e.getClass().getSimpleName()))
                 .message(e.getMessage())
                 .reason("Integrity constraint has been violated.")
                 .status(HttpStatus.CONFLICT.name())
@@ -52,12 +61,12 @@ public class ErrorHandler {
                 .build();
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiError handleInternalError(final Throwable e) {
         log.error("Internal error: {}", e.getMessage());
         return ApiError.builder()
-                .errors(Collections.singletonList(e.getStackTrace()[0].toString()))
+                .errors(Collections.singletonList(e.getClass().getSimpleName()))
                 .message(e.getMessage())
                 .reason("Error occurred")
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.name())
