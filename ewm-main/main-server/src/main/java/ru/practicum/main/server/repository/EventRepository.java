c2c9f46b61b024c7ef2fa82feb0c98e4e963e3ee
SkyDiver <lvv10@yandex.ru>
2026-03-04 11:35:20 +0300

    Исправление замечаний после ревью 1. fix build

--- ewm-main/main-server/src/main/java/ru/practicum/main/server/repository/EventRepository.java
+++ ewm-main/main-server/src/main/java/ru/practicum/main/server/repository/EventRepository.java
@@ -19,8 +19,8 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             "WHERE (:users IS NULL OR e.initiator.id IN :users) " +
             "AND (:states IS NULL OR e.state IN :states) " +
             "AND (:categories IS NULL OR e.category.id IN :categories) " +
-            "AND (CAST(:rangeStart AS timestamp) IS NULL OR e.eventDate >= :rangeStart) " +
-            "AND (CAST(:rangeEnd AS timestamp) IS NULL OR e.eventDate <= :rangeEnd)")
+            "AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) " +
+            "AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd)")
     List<Event> findEventsByAdmin(@Param("users") List<Long> users,
                                   @Param("states") List<EventState> states,
                                   @Param("categories") List<Long> categories,
@@ -35,7 +35,9 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             "AND (:categoriesEmpty = true OR e.category.id IN :categories) " +
             "AND (:paid IS NULL OR e.paid = :paid) " +
             "AND (e.eventDate BETWEEN :rangeStart AND :rangeEnd) " +
-            "AND (:onlyAvailable = false OR e.participantLimit = 0 OR e.confirmedRequests < e.participantLimit)")
+            "AND (:onlyAvailable = false OR e.participantLimit = 0 OR " +
+            "(SELECT count(pr.id) FROM ParticipationRequest pr " +
+            " WHERE pr.event.id = e.id AND pr.status = 'CONFIRMED') < e.participantLimit)")
     List<Event> findPublishedEvents(@Param("text") String text,
                                     @Param("categories") List<Long> categories,
                                     @Param("categoriesEmpty") boolean categoriesEmpty,
